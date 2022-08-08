05f5f20028c07f493be0951ae983da57b1ed3387
Seonwoo Kim <010620sun@naver.com>
2022-08-08 22:54:23 +0900

    third commit!!

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,10 +4,11 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-@Service
+@Service @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     @Autowired
