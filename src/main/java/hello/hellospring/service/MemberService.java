0a2632819ab0308fd9b1fe55d5164472c487b3df
Seonwoo Kim <010620sun@naver.com>
2022-08-09 00:08:09 +0900

    Last commit!!

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-@Service @Transactional
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     @Autowired
